f462dd4ab53e0532db571d761e8547b26866685c
mhughe43 <mhughe43@gmail.com>
2022-04-14 13:22:15 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedComponent } from './pages/feed/feed.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //http://localhost:4200/login
+  {path:"", component: LoginComponent},
+  //http://localhost:4200/register
+  {path:"register", component: RegisterComponent},
+  //http://localhost:4200/profile
+  {path:"profile", component: ProfileComponent},
+  //http://localhost:4200/feed
+  {path:"feed", component: FeedComponent},
+  //redirect to login?
+  {path:"**", redirectTo:""}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
