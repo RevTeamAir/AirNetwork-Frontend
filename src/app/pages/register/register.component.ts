0f4d97cd1c73d6f7590b79833a043109a93da94a
mhughe43 <mhughe43@gmail.com>
2022-04-18 19:48:37 -0600

    register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +9,29 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+    userId : number = 0
+  @Input()
+    firstname : string ="";
+    lastname : string ="";
+    username : string ="";
+    password : string ="";
+    email : string="";
+    profilePictureLocation: string="";
+    bio: string = "";
   
-  constructor() { }
+  constructor(private apiService : ApiService) { }
 
   ngOnInit(): void {
   }
 
-}
+  submit(createProfile: any){
+    this.apiService.createProfile(this.userId, this.firstname, this.lastname, this.username, this.password, this.email, this.profilePictureLocation, this.bio)
+    console.log("form submitted", createProfile)
+     
+    }
+
+    // createProfile(){
+    //   this.apiService.createProfile(this.userId, this.firstname, this.lastname, this.username, this.password, this.email, this.profilePictureLocation, this.bio)
+  
+    // }
+  }
